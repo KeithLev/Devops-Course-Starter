dc1c3392dc96d55d4ac868313f834ce279dedf45
KeithLev <74147088+KeithLev@users.noreply.github.com>
2022-01-06 12:48:37 +0000

    tried adding item amend

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template,request
 from flask.ctx import RequestContext
 from flask.wrappers import Request
 from werkzeug.utils import redirect
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import get_items, add_item, get_item, save_item
 
 from todo_app.flask_config import Config
 
@@ -17,6 +17,16 @@ def index():
 @app.route('/submit', methods=['POST','GET'])
 def submit():
     add_item(request.form.get('title'))
+    return redirect('/')
+
+@app.route('/todo/<id>')
+def view_item(id):
+    return render_template('item.html', ToDo = get_item(id))
+
+@app.route('/submit_item/<id>', methods=['POST', 'GET'])
+def submit_item(id):
+    item = {'id': id, 'status' : request.form.get('status'), 'title' : request.form.get('title')} 
+    save_item(item)
     return redirect('/')
 
     
