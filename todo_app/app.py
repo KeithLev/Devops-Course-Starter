a3d2470453c359d07eb37c8fb59efb7be48b3ba2
KeithLev <74147088+KeithLev@users.noreply.github.com>
2022-01-31 12:04:28 +0000

    Updated with changes suggest on pull request

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template,request
-from flask.ctx import RequestContext
-from flask.wrappers import Request
 from werkzeug.utils import redirect
 from todo_app.data.session_items import get_items, add_item, get_item, save_item
 
@@ -16,29 +14,26 @@ def index():
 
     items = sorted(items, key=lambda items : items['status'], reverse=True)
 
-    return render_template('index.html', ToDos = items)
+    return render_template('index.html', to_dos = items)
 
-@app.route('/submit', methods=['POST','GET'])
+@app.route('/add_item', methods=['POST'])
 def submit():
     add_item(request.form.get('title'))
     return redirect('/')
 
-@app.route('/todo/<id>')
+@app.route('/to_do/<id>')
 def view_item(id):
     item = get_item(id)
     not_started = False
     started = False
     done = False
 
-    if item['status'] == "Not Started":
-        not_started = True
-    if item['status'] == "Started":
-        started = True
-    if item['status'] == "Done":
-        done = True     
-    return render_template('item.html', ToDo = get_item(id), not_started  = not_started, started = started, done = done)
+    not_started = item['status'] == "Not Started"
+    started = item['status'] == "Started"
+    done = item['status'] == "Done" 
+    return render_template('item.html', to_do = get_item(id), not_started  = not_started, started = started, done = done)
 
-@app.route('/submit_item/<id>', methods=['POST', 'GET'])
+@app.route('/update_item/<id>', methods=['POST', 'GET'])
 def submit_item(id):
     item = get_item(id)
     item['title'] = request.form.get('title')
